669a53b5c1527ceb104b04190309092ddb0fa41b
yp23aai <yp23aai@herts.ac.uk>
2025-03-26 14:26:50 +0000

    Update Teamwork.java
    added our names

--- baths-students/src/wars/Teamwork.java
+++ baths-students/src/wars/Teamwork.java
@@ -17,25 +17,29 @@ public class Teamwork
         // Please list the member details alphabetically by surname 
         // i.e. the surname of member1 should come alphabetically 
         // before the surname of member 2...etc
-        details[0] = "team number";
+        details[0] = "85";
         
-        details[1] = "surname of member1";
-        details[2] = "first name of member1";
-        details[3] = "SRN of member1";
+        details[1] = "Martires";
+        details[2] = "Julia";
+        details[3] = "22096618";
 
-        details[4] = "surname of member2";
-        details[5] = "first name of member2";
-        details[6] = "SRN of member2";
+        details[4] = "Jordanbland";
+        details[5] = "Fred";
+        details[6] = "22059872";
 
-        details[7] = "surname of member3";
-        details[8] = "first name of member3";
-        details[9] = "SRN of member3";
+        details[7] = "Pranoto";
+        details[8] = "Yodhitomo";
+        details[9] = "23013218";
 
+        details[10] = "Hasan";
+        details[11] = "Nouh";
+        details[12] = "23021813";
 
-        details[10] = "surname of member4";
-        details[11] = "first name of member4";
-        details[12] = "SRN of member4";
-
+	
+	   // only if applicable
+        details[13] = "surname of member5";
+        details[14] = "first name of member5";
+        details[15] = "SRN of member5";
 	
 	   // only if applicable
         details[13] = "surname of member5";
